923bb7e7b6d6db8825cd5b5550033d5302891de8
Sung Jun <sungsikjun@gmail.com>
2025-05-02 07:40:41 -0400

    init Camera UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt") // kapt 플러그인 추가
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -33,10 +35,27 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        compose = false // Compose 비활성화
+    }
 }
 
 dependencies {
+    // Compose 의존성 제거
+    // implementation("androidx.compose.ui:ui:1.x.x") // 제거
+    // implementation("androidx.compose.material:material:1.x.x") // 제거
 
+    // View 시스템 및 프로젝트 의존성
+    implementation(libs.androidx.constraintlayout.v214)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +64,27 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+//    implementation("androidx.core:core-ktx:1.12.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    implementation("androidx.activity:activity-ktx:1.9.0")
+//    // Hilt
+//    implementation("com.google.dagger:hilt-android:2.48")
+//    kapt("com.google.dagger:hilt-android-compiler:2.48")
+//    // Retrofit
+//    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+//    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+//    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+//    // Coroutines
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+//    // LiveData
+//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/ssj/imageupload/HiltApplication.kt
@@ -0,0 +1,7 @@
+package com.ssj.imageupload
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application()
